37135160d55fb5f4f69e04244e4024a9fa9fbb0d
José Everaldo <jlima788@yahoo.com.br>
2015-02-27 21:47:00 -0300

    M6EX1 - Curso Zend Fundamentals Controller e Plugins

--- module/Market/src/Market/Controller/IndexController.php
+++ module/Market/src/Market/Controller/IndexController.php
@@ -1,21 +1,20 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/Market for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Market\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return array();
+        
+        $messages = array();
+        if($this->flashMessenger()->hasMessages()){
+            $messages = $this->flashMessenger()->getMessages();
+        }
+        return new ViewModel(array('messages'=>$messages));
     }
 
     public function fooAction()
