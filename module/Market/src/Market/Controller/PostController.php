25c40cc8a4e732a0b8cdf790c260e115e1869820
José Everaldo <jlima788@yahoo.com.br>
2015-03-11 14:28:33 -0300

    M8EX1 - Curso Zend Fundamentals Views e Helpers

--- module/Market/src/Market/Controller/PostController.php
+++ module/Market/src/Market/Controller/PostController.php
@@ -15,7 +15,10 @@ class PostController extends AbstractActionController {
 
     public function indexAction() 
     {
-        return new ViewModel(array('categories' => $this->categories));
+        $viewModel = new ViewModel(array('categories' => $this->categories));
+        $viewModel->setTemplate("market/post/invalid.phtml");
+        
+        return $viewModel;
     }
 
 }
